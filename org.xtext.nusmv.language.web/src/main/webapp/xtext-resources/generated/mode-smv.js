4028c04dd0971c25fefda002f882ca859b28d6ff
liamwalsh <liam.walsh@queensu.ca>
2020-09-27 11:41:34 -0400

    this version compiles but language support does not function

--- org.xtext.nusmv.language.web/src/main/webapp/xtext-resources/generated/mode-smv.js
+++ org.xtext.nusmv.language.web/src/main/webapp/xtext-resources/generated/mode-smv.js
@@ -3,10 +3,6 @@ define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], functi
 		var keywords = "A|ABF|ABG|AF|AG|ASSIGN|AX|COMPUTE|CONSTANTS|CTLSPEC|DEFINE|E|EBF|EBG|EF|EG|EX|F|FAIRNESS|FALSE|FROZENVAR|G|H|INIT|INVAR|INVARSPEC|ISA|IVAR|JUSTICE|LTLSPEC|MAX|MIN|MODULE|NAME|O|PSLSPEC|S|SPEC|T|TRANS|TRUE|U|V|VAR|X|Y|Z|array|bool|boolean|case|esac|in|init|mod|next|of|process|self|toint|union|unsigned|word|word1|xnor|xor";
 		this.$rules = {
 			"start": [
-				{token: "comment", regex: "(--).*$"},
-				{token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
-				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
-				{token: "constant.numeric", regex: "\\b[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
 				{token: "lparen", regex: "[\\[({]"},
 				{token: "rparen", regex: "[\\])}]"},
 				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
